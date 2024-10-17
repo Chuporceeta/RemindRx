4e123f149ca3f7396929e335831133425d3a56ad
Xabier <xabier.sienra@gmail.com>
2024-10-17 14:27:14 -0400

    Create firebase-config.ts

---
+++ firebase-config.ts
@@ -0,0 +1,12 @@
+// These are fine to have public
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyD076ENRDRauDUNj0bTZURJDlf26LmJmA0",
+    authDomain: "remindrx-1c8dd.firebaseapp.com",
+    projectId: "remindrx-1c8dd",
+    storageBucket: "remindrx-1c8dd.appspot.com",
+    messagingSenderId: "966406512713",
+    appId: "1:966406512713:web:1f46bc061b0249e17d5f98"
+};
+
+export const vapidKey = 'BBwMxByKR54lgFmD865aGRvDM9vLngLjZqeAgQ_qKcAdzp2VA3HZF7t5XXoXs87kdfSG2antBI7KNLYoqt3rOGo';
