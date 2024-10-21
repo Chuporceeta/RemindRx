68ecdfe1fedc67390aff579a2439cf5bc34979da
kartik-199 <kathuriakartik2005@gmail.com>
2024-10-21 15:05:45 -0400

    code formatting changes

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -1,90 +1,30 @@
 import {Link, useNavigate} from 'react-router-dom';
 import {useState} from 'react';
-import {
-    Text, 
-    Stack, 
-    PrimaryButton, 
-    TextField,
-    IStackStyles, 
-    IStackTokens
-} from '@fluentui/react'
+import {Text, Stack, PrimaryButton, TextField, IStackStyles, IStackTokens} from '@fluentui/react'
 function WelcomePage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-    const stackStyle: IStackStyles = {
-        root: {
-            width: '150%',
-            maxWidth: '400px',
-            margin: '0 auto', 
-            padding: '20px'
-        }
+    const stackS: IStackStyles = {
+        root: {width: '150%', maxWidth: '400px', margin: '0 auto', padding: '20px'}
     };
-    const stackToken: IStackTokens = {
-        childrenGap: 25, 
-        padding: 20
-    };
+    const stackTkn: IStackTokens = {childrenGap: 25, padding: 20};
     const buttonStyle = {
-        root: {
-            margin: '0 8px',
-            border: 'none'
-        },
-        rootHovered:{
-             backgroundColor: '#005a9e',
-             border: 'none'
-        }
+        root: {margin: '0 8px', border: 'none'},
+        rootHovered:{backgroundColor: '#005a9e', border: 'none'}
     };
     const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        /*
-        ------------------Login Auth Logic ------------------
-        const loginAuthEndpoint = "OUR_API_BACKEND_CONNECTOR"
-        const loginData = {
-            username: username,
-            password: password
-        };
-        try {
-            const response = await fetch(loginAuthEndpoint, {
-                method: 'POST', 
-                headers:{
-                    'Content-Type' : 'application/json'
-                },
-                body: JSON.stringify(loginData)
-            });
-            const data = await response.json()
-            if(response.ok){
-                localStorage.setItem('authToken', data.token);
-                navigate('/home')
-            }
-            else{
-                console.log('Login failed: ', data.message)
-            }
-        }
-        catch (error){
-            console.error('Error during login:', error)
-        }
-        */
-       navigate('/home');
+        navigate('/home');
     };
     return (
         <Stack 
             horizontalAlign="center"
             verticalAlign="center"
-            styles={{
-                root: {
-                    minHeight: '100vh',
-                    backgroundColor: '#f0f0f0'
-                }
-            }}
+            styles={{root: {minHeight: '100vh', backgroundColor: '#f0f0f0'}}}
         >
-            <Stack
-                horizontalAlign="center"
-                tokens={stackToken}
-                styles={stackStyle}
-            >
-                <Text variant="xxLarge" block styles={{root: {fontSize: '60px', fontWeight: '900'}}}>
-                    RemindRX
-                </Text>
+            <Stack horizontalAlign="center" tokens={stackTkn} styles={stackS}>
+                <Text variant="xxLarge" block styles={{root: {fontSize: '60px', fontWeight: '900'}}}>RemindRX</Text>
                 <Stack
                     styles={{
                         root:{
@@ -106,7 +46,6 @@ function WelcomePage() {
                                 onChange={(_, newValue) => setUsername(newValue || '')}
                                 required
                             />
-
                             <TextField
                                 type="password"
                                 placeholder="Password"
@@ -115,18 +54,10 @@ function WelcomePage() {
                                 canRevealPassword
                                 required
                             />
-
                             <Stack horizontal horizontalAlign="center" tokens={{ childrenGap: 6 }}>
-                                <PrimaryButton
-                                    text="Login"
-                                    type="submit"
-                                    styles={buttonStyle}
-                                />
+                                <PrimaryButton text="Login" type="submit" styles={buttonStyle}/>
                                 <Link to="/create-account" style={{ textDecoration: 'none' }}>
-                                <PrimaryButton
-                                    text="Create Account"
-                                    styles={buttonStyle}
-                                />
+                                    <PrimaryButton text="Create Account" styles={buttonStyle}/>
                                 </Link>
                             </Stack>
                         </Stack>
@@ -137,5 +68,4 @@ function WelcomePage() {
 
     );
 }
-
 export default WelcomePage;
