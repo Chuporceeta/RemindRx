9f06d7aac1ae69b57af0a496a2b6582c9b91a58f
kartik-199 <kathuriakartik2005@gmail.com>
2024-11-18 00:46:24 -0500

    Edit account + export functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import CreateAccount from './components/CreateAcct'
 import HomePage from './components/HomePage.tsx'
 import EditMed from './components/EditMed.tsx'
 import CreateMed from './components/CreateMed.tsx'
+import EditAcct from './components/EditAcct.tsx';
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
               <Route path="/home" element={<HomePage/>} />
               <Route path="/create-med" element={<CreateMed/>}/>
               <Route path="/edit-med" element={<EditMed/>}/>
+              <Route path="/edit-account" element={<EditAcct/>}/>
       </Routes>
     </Router>
 
